e930527d12aeef62cadb7b5e8bc199b9d5c256f7
Frank von Hoven <fvonhoven@gmail.com>
2023-02-10 19:55:45 -0600

    Update build-client baseUrl

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://www.ticketzone.online",
+      baseURL: "https://www.ticketzone.online",
       // baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     })
